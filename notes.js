219092c799bd213322f4dc69ac557c58487e28bf
stanciudragosioan <stanciudragosioan92@gmail.com>
2020-09-07 17:06:37 +0300

    1st commit

---
+++ notes.js
@@ -0,0 +1,162 @@
+let notes;
+
+if (localStorage.getItem("notes") === null) {
+  notes = [];
+} else {
+  notes = JSON.parse(localStorage.getItem("notes"));
+}
+
+/*
+ * Grabs user note and saves to localStorage API
+ */
+const grabNote = (e) => {
+  const note = {};
+  let noteText = document.querySelector("#note").value;
+  note.text = noteText;
+  const noteDate = new Date()
+    .toString()
+    .replace(/\S+\s(\S+)\s(\d+)\s(\d+)\s.*/, "$2-$1-$3");
+  note.date = noteDate;
+  let id =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+  note.id = id;
+  notes.push(note);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  document.querySelector("#note").value = "";
+  displayNotes();
+};
+
+let saveBtn = document.querySelector(".add-note");
+saveBtn.addEventListener("click", grabNote);
+
+/*
+ * Grabs notes from localStorage API and displays them to UI
+ */
+const displayNotes = () => {
+  let records = document.querySelector("#records");
+  let output = "";
+  notes.map((n) => {
+    output += `
+        <div class="card">
+            <p class="text">
+                ${n.text}
+            </p>
+
+            <p class="text">
+                Written at: ${n.date}
+            </p>
+            <p class="text invisible">
+                id: ${n.id}
+            </p>
+            <i class="fa fa-pencil pencil-note" aria-hidden="true"></i>
+            <span id="saveEdit"></span><i class="fa fa-times-circle" aria-hidden="true"></i>
+            <hr>
+            
+        </div>
+      `;
+  });
+  records.innerHTML = output;
+};
+
+displayNotes();
+
+let editBtn = Array.from(document.querySelectorAll(".pencil-note"));
+
+const editNote = (e) => {
+  let idToEdit = e.target.parentElement.childNodes[5].innerText
+    .trim()
+    .split(":")[1]
+    .trim();
+  let cardToEdit = e.target.parentElement.childNodes[1];
+  cardToEdit.contentEditable = "true";
+  cardToEdit.style.backgroundColor = "#fff";
+  cardToEdit.style.padding = "1.5rem";
+  const editIcon = e.target.parentElement.childNodes[9];
+  editIcon.style.display = "inline-block";
+  editIcon.innerHTML = `<i class="fa fa-check" aria-hidden="true"></i>`;
+  e.preventDefault();
+};
+
+const saveNote = (e) => {
+  let newText = e.target.parentElement.parentElement.childNodes[1].innerText.trim();
+  let newDate = new Date()
+    .toString()
+    .replace(/\S+\s(\S+)\s(\d+)\s(\d+)\s.*/, "$2-$1-$3");
+  let idToEdit = e.target.parentElement.parentElement.childNodes[5].innerText
+    .trim()
+    .split(":")[1]
+    .trim();
+
+  let newResource = {
+    text: newText,
+    date: newDate,
+    id: idToEdit,
+  };
+
+  notes.map((n, index) => {
+    if (n.id === idToEdit) {
+      notes[index] = newResource;
+    }
+  });
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+  let cardToEdit = e.target.parentElement.parentElement.childNodes[1];
+  cardToEdit.contentEditable = "false";
+  cardToEdit.style.backgroundColor = "#f0edd9";
+  cardToEdit.style.padding = "0";
+  const editIcon = e.target.parentElement.parentElement.childNodes[9];
+  editIcon.style.display = "none";
+};
+
+const saveIcon = Array.from(document.querySelectorAll("#saveEdit"));
+saveIcon.map((i) => {
+  i.addEventListener("click", saveNote);
+});
+
+editBtn.map((i) => {
+  i.addEventListener("click", editNote);
+});
+
+/*
+ * Deletes a note
+ */
+const deleteNote = (e) => {
+  let idToDelete = e.target.parentElement.childNodes[5].innerText
+    .split(":")[1]
+    .trim();
+  notes.map((n, index) => {
+    if (n.id === idToDelete) {
+      notes.splice(index, 1);
+    }
+  });
+  localStorage.setItem("notes", JSON.stringify(notes));
+  e.target.parentElement.style.display = "none";
+};
+let deleteBtns = Array.from(document.querySelectorAll(".fa-times-circle"));
+deleteBtns.map((b) => {
+  b.addEventListener("click", deleteNote);
+});
+
+/*
+ * filters notes
+ */
+
+const filter = () => {
+  //filter by title, subreddit, author
+  document.querySelectorAll(".card").forEach((item) => {
+    let value = document.querySelector(".filter").value.toLowerCase();
+
+    const note = item.children[0].innerText;
+    const date = item.children[1].innerText;
+
+    if (
+      note.toLowerCase().indexOf(value) != -1 ||
+      date.toLowerCase().indexOf(value) != -1
+    ) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+};
---
+++ ui.js
@@ -0,0 +1,79 @@
+//buttons & inputs
+const inputBtn = document.querySelector("#inputBtn");
+const input = document.querySelector("#input");
+const list = document.querySelector("#records");
+const listBtn = document.querySelector("#recordsBtn");
+const recordsBtn = document.querySelector("#recordsBtn");
+//colors for UI customisation
+const c1 = document.getElementById("c1");
+const c2 = document.getElementById("c2");
+const c3 = document.getElementById("c3");
+//colors for customize
+const bodyBackground = document.querySelector("body");
+
+let btn1Color = "#050D31";
+let btn2Color = "#2600FF";
+
+//methods
+const toggleInput = () => {
+  input.style.display = "block";
+  list.style.display = "none";
+  inputBtn.style.backgroundColor = btn1Color;
+  listBtn.style.backgroundColor = btn2Color;
+  document.querySelector(".paper").style.display = "block";
+  document.querySelector(".filter").style.display = "none";
+};
+
+const toggleRecords = () => {
+  list.style.display = "block";
+  input.style.display = "none";
+  listBtn.style.backgroundColor = btn1Color;
+  inputBtn.style.backgroundColor = btn2Color;
+  document.querySelector(".filter").style.display = "block";
+};
+
+const swapToBlue = () => {
+  console.log("blue theme here");
+  bodyBackground.style.backgroundColor = "#6C8CE2";
+  btn1Color = "#050D31";
+  btn2Color = "#2600FF";
+  inputBtn.style.backgroundColor = btn2Color;
+  recordsBtn.style.backgroundColor = btn2Color;
+  listBtn.style.border = "none";
+  inputBtn.style.border = "none";
+  listBtn.style.border = `2px solid ${btn1Color}`;
+  inputBtn.style.border = `2px solid ${btn1Color}`;
+};
+
+const swapToYellow = () => {
+  console.log("yellow theme here");
+  bodyBackground.style.backgroundColor = "#fcce03";
+  btn1Color = "#ad871d";
+  btn2Color = "#ebc252";
+  inputBtn.style.backgroundColor = btn2Color;
+  recordsBtn.style.backgroundColor = btn2Color;
+  listBtn.style.border = "none";
+  inputBtn.style.border = "none";
+  listBtn.style.border = `2px solid ${btn1Color}`;
+  inputBtn.style.border = `2px solid ${btn1Color}`;
+};
+
+const swapToPink = () => {
+  console.log("pink theme here");
+  bodyBackground.style.backgroundColor = "pink";
+  btn1Color = "#97089e";
+  btn2Color = "#f003fc";
+  inputBtn.style.backgroundColor = btn2Color;
+  recordsBtn.style.backgroundColor = btn2Color;
+  listBtn.style.border = "none";
+  inputBtn.style.border = "none";
+  listBtn.style.border = `2px solid ${btn1Color}`;
+  inputBtn.style.border = `2px solid ${btn1Color}`;
+};
+
+//event listeners
+inputBtn.addEventListener("click", toggleInput);
+listBtn.addEventListener("click", toggleRecords);
+c1.addEventListener("click", swapToBlue);
+c2.addEventListener("click", swapToYellow);
+c3.addEventListener("click", swapToPink);
